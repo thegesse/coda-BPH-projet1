7ff66bf265f35b9ca0ea29905fe3a2e9453c92b2
Esteban Quillerier <emquillerier@gmail.com>
2025-11-23 15:14:46 +0100

    corrected comments

--- choose-your-adventure/controllers/map-attributes.php
+++ choose-your-adventure/controllers/map-attributes.php
@@ -3,16 +3,16 @@
     foreach ($storyMap as $nodeId => $nodeData) { //foreach to access and assign the outer parts of the array ex: ID and texte
         $ID  = $nodeId;
         $nodeText = $nodeData["texte"];
-        $nodeChoices = [];
+        $nodeChoices = []; //initialize array to store the choices
         foreach ($nodeData["choices"] as $choiceArray) { //forheach to assign the array of choices inside each node
-            foreach ($choiceArray as $choiceTexte => $nextNodeId) {
+            foreach ($choiceArray as $choiceTexte => $nextNodeId) { 
                 $nodeChoices[] = [
                     "text" => $choiceTexte,
                     "nextId" => $nextNodeId
                 ];
             }
         }
-        $mapAttributes[$ID] = [ //to store all the date
+        $mapAttributes[$ID] = [ //to store all the data for the fucnction
         "id" => $ID,
         "texte" => $nodeText,
         "choices" => $nodeChoices
@@ -20,7 +20,7 @@
 
     }
 
-    function getData($nodeId) { //stole from ai to not repeatedly redefine all the data on every new node change
+    function getData($nodeId) { //stole from ai to not define the entirety of the data in the index file 
         global $mapAttributes; //global used to access stuff defined outside of the function
         return isset($mapAttributes[$nodeId]) ? $mapAttributes[$nodeId] : false;
     }
